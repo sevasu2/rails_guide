83aae9afdde9c8965ba34c6347e763fd44ce1254
Sevasu <sevasunishimura@gmail.com>
2021-02-11 18:47:32 +0900

    article詳細

--- src/app/controllers/articles_controller.rb
+++ src/app/controllers/articles_controller.rb
@@ -1,21 +1,22 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.all
   end
 
   def new
   end
 
   def create
-    @article = Article.new(params[:article])
+    @article = Article.new(article_params)
     @article.save
     redirect_to @article
-    # render plain: params[:article].inspect
   end
 
   def edit
   end
 
   def show
+    @article = Article.find(params[:id])
   end
 
   private
